d543f8ef5ebed2a91de630b50fddb182f55448e4
remonagayby <agayby.remon@gmail.com>
2024-10-20 17:35:20 +0300

    script setup

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # cypress-ecommerce-automation-script
+
+## Setup 🛠️
+
+### Pre-requisites
+
+* Install NodeJS and NPM pakcage manager.
+* Code Editor of your choice e.g. Visual Studio Code
+
+### Setup from Scratch
+
+* `npm init` to setup node project with package.json
+* `npm install --save-dev cypress` to install cypress as dev dependency
---
+++ cypress.config.js
@@ -0,0 +1,15 @@
+const { defineConfig } = require("cypress");
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: "https://www.automationexercise.com",
+    video: true,
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+      allureWriter(on, config);
+      return config;
+
+    },
+  },
+});
